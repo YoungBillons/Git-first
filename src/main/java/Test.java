d98351fda4a739a9477534e8cfd6a15ac71cf28f
MyYoung <ysh18937018369@163.com>
2021-11-29 10:21:09 +0000

    update src/main/java/Test.java.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,5 +4,12 @@ public class Test {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello word");
+        public static makeDir(String name){
+            if(" ".equals(name)){
+                System.out.println("这啥啊这是！啊！");
+            }else{
+                 System.out.println("有人吗？")
+            }
+        }
     }
 }
