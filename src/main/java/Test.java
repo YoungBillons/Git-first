da570756cdd7fd4b0879596604dd32428de07bc2
MyYoung <a15998123772@qq.com>
2021-11-29 18:22:53 +0800

    fourth commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,15 +1,11 @@
+
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello word");
-        public static makeDir(String name){
-            if(" ".equals(name)){
-                System.out.println("这啥啊这是！啊！");
-            }else{
-                 System.out.println("有人吗？")
-            }
-        }
+
     }
 }
